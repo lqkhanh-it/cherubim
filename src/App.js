6f831e6e7005ae2d6e28a7661d63a25451c33023
khanh.le <lequockhanh292@gmail.com>
2022-10-04 15:41:29 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Dashboard from "./pages/dashboard";
+import About from "./pages/about";
+import Testimonial from "./pages/testimonial";
 import BackTop from "./components/backtop";
 import Header from "./pages/header/index";
 import Footer from "./pages/footer/index";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Dashboard />
+      <Route path="/" component={Dashboard} />
+      <Route path="/about" component={About} />
+      <Route path="/testimonial" component={Testimonial} />
       <Footer />
       <BackTop />
     </div>
