31dca7e3003dd5ebafe5c5e87e3f55827e633086
khanh.le1 <khanh.le1@mservice.com.vn>
2022-10-08 10:40:49 +0700

    done

--- src/pages/banner/index.jsx
+++ src/pages/banner/index.jsx
@@ -9,61 +9,48 @@ const Banner = () => {
         className="carousel slide"
         data-bs-ride="carousel"
       >
-        <div className="carousel-inner" style={{ height: "100vh" }}>
-          <div className="carousel-item active">
-            <img className="w-100 h-100" src={image1} alt="aa" />
-            <div className="carousel-caption">
-              <div className="container">
-                <div className="row justify-content-center">
-                  <div className="col-lg-7 pt-5">
-                    <h1 className="display-4 text-white mb-3 animated slideInDown">
-                      Let's Change The World With Humanity
-                    </h1>
-                    <p className="fs-5 text-white-50 mb-5 animated slideInDown">
-                      Aliqu diam amet diam et eos. Clita erat ipsum et lorem sed
-                      stet lorem sit clita duo justo erat amet
-                    </p>
-                    <a
-                      className="btn btn-primary py-2 px-3 animated slideInDown"
-                      href="#"
-                    >
-                      Learn More
-                      <div className="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
-                        <i className="fa fa-arrow-right"></i>
+        <div
+          className="carousel-inner"
+          style={{ maxHeight: "100vh", minHeight: "100vh" }}
+        >
+          {[1, 2].map((key) => {
+            return (
+              <div
+                className={`carousel-item ${key % 2 === 0 ? "active" : ""}`}
+                key={key}
+              >
+                <img
+                  className="w-100 h-100"
+                  src={key % 2 === 0 ? image1 : image2}
+                  alt="aa"
+                />
+                <div className="carousel-caption">
+                  <div className="container">
+                    <div className="row justify-content-center">
+                      <div className="col-lg-7 pt-5">
+                        <h1 className="display-4 text-white mb-3 animated slideInDown">
+                          Let's Change The World With Humanity
+                        </h1>
+                        <p className="fs-5 text-white-50 mb-5 animated slideInDown">
+                          Aliqu diam amet diam et eos. Clita erat ipsum et lorem
+                          sed stet lorem sit clita duo justo erat amet
+                        </p>
+                        <a
+                          className="btn btn-primary py-2 px-3 animated slideInDown"
+                          href="#"
+                        >
+                          Learn More
+                          <div className="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
+                            <i className="fa fa-arrow-right"></i>
+                          </div>
+                        </a>
                       </div>
-                    </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img className="w-100 h-100" src={image2} alt="aaa" />
-            <div className="carousel-caption">
-              <div className="container">
-                <div className="row justify-content-center">
-                  <div className="col-lg-7 pt-5">
-                    <h1 className="display-4 text-white mb-3 animated slideInDown">
-                      Let's Save More Lifes With Our Helping Hand
-                    </h1>
-                    <p className="fs-5 text-white-50 mb-5 animated slideInDown">
-                      Aliqu diam amet diam et eos. Clita erat ipsum et lorem sed
-                      stet lorem sit clita duo justo erat amet
-                    </p>
-                    <a
-                      className="btn btn-primary py-2 px-3 animated slideInDown"
-                      href="#"
-                    >
-                      Learn More
-                      <div className="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
-                        <i className="fa fa-arrow-right"></i>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
         <button
           className="carousel-control-prev"
