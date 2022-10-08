5f9104bb4e1b5c123659748eb03c56ca569ec83a
khanh.le <lequockhanh292@gmail.com>
2022-10-08 21:55:22 +0700

    aaaa

--- src/pages/banner/index.jsx
+++ src/pages/banner/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-import image1 from "../../img/default.jpeg";
+import defaultImage from "../../img/default.jpeg";
 
 const Banner = () => {
+  const data = [
+    {
+      id: 1,
+      title: "Mùa lễ Halloween sắp tới",
+      description:
+        "Cùng Cherubim đón chờ một tháng 10 bùng nổ và nhiều ưu đãi nhé!!",
+      image:
+        "https://i0.wp.com/www.myuna.ca/assets/media/2020/10/Halloween-2021-banner-01.png?fit=1920%2C1080&ssl=1",
+      link: "#",
+      theme: "light",
+      float: "start",
+    },
+  ];
+
+  const heightConfig = { maxHeight: "100vh", minHeight: "100vh" };
+  const textColor = data[0]?.theme === "light" ? "text-white" : "text-primary";
+
   return (
     <div className="container-fluid p-0 mb-5">
       <div
@@ -9,73 +26,81 @@ const Banner = () => {
         className="carousel slide"
         data-bs-ride="carousel"
       >
-        <div
-          className="carousel-inner"
-          style={{ maxHeight: "70vh", minHeight: "70vh" }}
-        >
-          {[1, 2].map((key) => {
-            return (
-              <div
-                className={`carousel-item ${key % 2 === 0 ? "active" : ""}`}
-                key={key}
-              >
-                <img
-                  className="w-100 h-100"
-                  src={key % 2 === 0 ? image1 : image1}
-                  alt="aa"
-                />
-                <div className="carousel-caption">
-                  <div className="container">
-                    <div className="row justify-content-center">
-                      <div className="col-lg-7 pt-5">
-                        <h1 className="display-4 text-white mb-3 animated slideInDown">
-                          Let's Change The World With Humanity
-                        </h1>
-                        <p className="fs-5 text-white-50 mb-5 animated slideInDown">
-                          Aliqu diam amet diam et eos. Clita erat ipsum et lorem
-                          sed stet lorem sit clita duo justo erat amet
-                        </p>
-                        <a
-                          className="btn btn-primary py-2 px-3 animated slideInDown"
-                          href="#"
-                        >
-                          Learn More
-                          <div className="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
-                            <i className="fa fa-arrow-right"></i>
-                          </div>
-                        </a>
+        <div className="carousel-inner" style={heightConfig}>
+          {data &&
+            data.length > 0 &&
+            data.map((item, key) => {
+              const { title, description, image, link, float } = item;
+              return (
+                <div
+                  className={`carousel-item ${key === 0 ? "active" : ""}`}
+                  key={item?.id}
+                  style={heightConfig}
+                >
+                  <img
+                    className="w-100 h-100"
+                    src={image}
+                    alt="Cherubim ưu đãi"
+                  />
+                  <div className="carousel-caption">
+                    <div className="container">
+                      <div className={`row justify-content-${float}`}>
+                        <div className={`col-lg-7 pt-2 text-${float}`}>
+                          <h1
+                            className={`display-4 ${textColor} mb-2 animated slideInDown`}
+                          >
+                            {title}
+                          </h1>
+                          <p
+                            className={`fs-5 ${textColor} mb-3 animated slideInDown`}
+                          >
+                            {description}
+                          </p>
+                          <a
+                            className="btn btn-cherubim py-2 px-3 animated slideInDown"
+                            href={link}
+                          >
+                            Cùng đi nào
+                            <div className="d-inline-flex btn-sm-square bg-white text-cherubim rounded-circle ms-2">
+                              <i className="fa fa-arrow-right"></i>
+                            </div>
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#header-carousel"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#header-carousel"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+        {data && data?.length > 1 && (
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#header-carousel"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+        )}
+        {data && data?.length > 1 && (
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#header-carousel"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        )}
       </div>
     </div>
   );
